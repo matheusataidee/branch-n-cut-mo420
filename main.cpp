1b9be94f51e6b9f5f3d8ef99dc11c579e62b0f0e
Matheus Ataide <matheusataide95@gmail.com>
2019-12-01 17:25:09 -0300

    Adding hybrid restrictions

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <map>
 #include <string>
+#include <fstream>
 
 #include "ilcplex/ilocplex.h"
 
@@ -44,7 +45,6 @@ int main(int argc, char * argv[]) {
   ModelType model_type;
   double timelimit;
   bool use_primal_heur;
-  string input_path;
 
   if (argc < 5 || argc > 5) {
     cout << "Usage: " << string(argv[0]) << " <model> <time-limit> <heur-primal> <arq-input>" << endl;
@@ -53,65 +53,113 @@ int main(int argc, char * argv[]) {
     cout << "\t<heur-primal>: 1 for using, 0 for not using" << endl;
     cout << "\t<arq-input>: path to input instance" << endl;
     return -1;
-  } else {
-    model_type = (string(argv[1]) == "s") ? ILP : HYBRID;
-    timelimit = atof(argv[2]);
-    use_primal_heur = (string(argv[3]) == "1");
-    input_path = string(argv[4]);
   }
+  model_type = (string(argv[1]) == "s") ? ILP : HYBRID;
+  timelimit = atof(argv[2]);
+  use_primal_heur = (string(argv[3]) == "1");
+  ifstream fin(argv[4]);
 
   /* ambiente do cplex */
   IloEnv env;
 
-  /* inicializa valores de variaveis globais */
-  totcuts=0;   itersep=0;
+  /* le dados de entrada do problema da AGMR.*/
+  fin >> v_ >> a_;
 
-  cout << "[Entrar com os dados da instancia:]" << endl;
-
-  /* le dados de entrada do problema da Mochila 0-1.*/
-  scanf("%d %d",&v_,&a_);
+  vector<vector<int> > g(v_, vector<int>());
+  map<pair<int, int>, int> edge_to_index;
   v.resize(v_);
   origem.resize(a_);
   destino.resize(a_);
 
-  for(int i=0;i<v_;i++) scanf("%d",&v[i]);
-  for(int i=0;i<a_;i++) scanf("%d %d",&origem[i],&destino[i]);
+  for (int i = 0; i < a_; i++) {
+    fin >> origem[i] >> destino[i];
+    origem[i]--;  destino[i]--; // Tornando a indexação 0-based
+    g[origem[i]].push_back(destino[i]);
+    g[destino[i]].push_back(origem[i]);
+    edge_to_index[{origem[i], destino[i]}] = 2 * i;
+    edge_to_index[{destino[i], origem[i]}] = 2 * i + 1;
+  }
 
   /* objeto que representa o modelo */
   IloModel model(env);
 
-  map<int, IloBoolVar*> y;
-  IloBoolVarArray y_temp(env, v_);
+  int root = 0;
+  map<int, IloBoolVar*> y_map;
+  IloBoolVarArray y(env, v_);
   for(int i = 0; i < v_; i++){
-    y[i] = &y_temp[i];
+    y_map[i] = &y[i];
   }
-
-  map<std::string, IloBoolVar*> x;
-  IloBoolVarArray x_temp(env, a_);
+  map<pair<int, int>, IloBoolVar*> x_map;
+  map<pair<int, int>, IloBoolVar*> z_map;
+  IloBoolVarArray x(env, a_);
+  IloBoolVarArray z(env, 2 * a_);
   for(int i = 0; i < a_; i++){
-    x["" + to_string(origem[i]) + "-" + to_string(destino[i]) + ""] = &x_temp[i];
+    x_map[{origem[i], destino[i]}] = &x[i];
+    z_map[{origem[i], destino[i]}] = &z[2 * i];
+    z_map[{destino[i], origem[i]}] = &z[2 * i + 1];
   }
 
   /* funcao objetivo (1) */
   IloExpr obj(env);
   for(int i = 0; i < v_; i++)
-    obj += *y[i];
+    obj += y[i];
   model.add(IloMinimize(env, obj)); 
 
   /* restricao (3) */
   IloExpr constr_3(env);
   for(int i = 0; i < a_; i++)
-    constr_3 += *x[to_string(origem[i]) + "-" + to_string(destino[i])];
+    constr_3 += x[i];
   model.add(constr_3 == v_ - 1);
 
   /* restricao (4) */
   for(int i = 0; i < v_; i++){
     IloExpr constr_4(env);
-    for(int e = 0; e < a_; e++){
-      if(i == origem[e]) constr_4 += *x[to_string(i) + "-" + to_string(destino[e])];
-      if(i == destino[e]) constr_4 += *x[to_string(origem[e]) + "-" + to_string(i)];
+    for (int e = 0; e < g[i].size(); e++) {
+      int to = g[i][e];
+      constr_4 += x[edge_to_index[{i, to}] / 2];
     }
-    model.add(constr_4 - 2 - 100 * *y[i] <= 0);
+    model.add(constr_4 - 2 <= ((int)g[i].size() - 2) * y[i]);
+  }
+
+  /* restricao (27) */
+  for (int i = 0; i < v_; i++) {
+    if (i == root) continue;
+    IloExpr constr_27(env);
+    for (int e = 0; e < g[i].size(); e++) {
+      int to = g[i][e];
+      /* Somando arestas de entrada */
+      constr_27 += z[edge_to_index[{to, i}]];
+    }
+    model.add(constr_27 == 1);
+  }
+
+  /* restricao (28) */
+  for (int i = 0; i < v_; i++) {
+    if (i == root) continue;
+    IloExpr constr_28(env);
+    for (int e = 0; e < g[i].size(); e++) {
+      int to = g[i][e];
+      /* Somando arestas de saida */
+      constr_28 += z[edge_to_index[{i, to}]];
+    }
+    model.add(constr_28 - 1 <= ((int)g[i].size() - 2) * y[i]);
+  }
+
+  /* restricao (29) */
+  IloExpr constr_29(env);
+  for (int e = 0; e < g[root].size(); e++) {
+    int to = g[root][e];
+    constr_29 += z[edge_to_index[{root, to}]];
+  }
+  model.add(constr_29 - 2 <= ((int)g[root].size() - 2) * y[root]);
+
+  /* restricao (35) */
+  IloExpr constr_35(env);
+  for (int i = 0; i < a_; i++) {
+    IloExpr constr_35(env);
+    constr_35 += z[2 * i];
+    constr_35 += z[2 * i + 1];
+    model.add(constr_35 == x[i]);
   }
 
   /* cria objeto do cplex */
@@ -128,23 +176,18 @@ int main(int argc, char * argv[]) {
   cout << "solucao otima: " << cplex.getObjValue() << endl;
 
   IloNumArray ystar(env);
-  cplex.getValues(ystar, y_temp);
+  cplex.getValues(ystar, y);
   cout << "valores de y:" << endl;
   for(int i = 0; i < v_; i++){
     cout << "y[" << i << "]: " << ystar[i] << endl;
   }
 
   IloNumArray xstar(env);
-  cplex.getValues(xstar, x_temp);
+  cplex.getValues(xstar, x);
   cout << "valores de x:" << endl;
   for(int i = 0; i < a_; i++){
     cout << "x[" << origem[i] << "," << destino[i] << "]: " << xstar[i] << endl;
   }
-
-  /* cria variaveis do modelo */
-/*  IloBoolVarArray y(env, v_); */
-
-/*  IloBoolVar x(env);  */
-
+  
   return 0;
 }
