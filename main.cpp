a586a1b59352c0179458457afd1e3ef7f64e0b53
Matheus Ataide <matheusataide95@gmail.com>
2019-12-02 19:32:53 -0300

    Adicionando algoritmo de min-cut

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <map>
 #include <string>
 #include <fstream>
+#include <queue>
+#include <cstdlib>
 
 #include "ilcplex/ilocplex.h"
 
@@ -38,6 +40,138 @@ enum ModelType {
   HYBRID
 };
 
+// DSU (Disjoint-Set-Union, or Union-Find)  
+// creates a new set consist of only the element v 
+void makeSet (int v, vector<int>& r, vector<int>& p) { 
+    p[v] = v; r[v] = 0; 
+} 
+// returns representative of the set in which v lies 
+int findSet (int v, vector<int>& r, vector<int>& p) {
+    return v == p[v] ? v : p[v] = findSet(p[v], r, p);
+}
+
+// unite the sets in which a and b lie
+int uniteSets (int a, int b, vector<int>& r, vector<int>& p) {
+    a = findSet(a, r, p);
+    b = findSet(b, r, p);
+    if (a != b) {
+        if (r[a] < r[b]) {
+            swap(a, b);
+        }
+        p[b] = a; 
+        if (r[a] == r[b]) {
+            r[a]++;
+        } 
+        return 1;
+    } else {
+      return 0;
+    }
+}
+
+/* Verifica se as variaveis estao coerentes e correspondem a uma arvore
+  retorna 1 quando ha inconsistencia nas variaveis z
+  retorna 2 quando o numero de arestas eh diferente de |v| - 1
+  retorna 3 quando ha inconsistencia nas variaveis y
+  retorna 4 quando o grafo nao é conexo
+  retorna 0 quando o as variaveis estao consistentes e correspondem a uma arvore */
+int testOk(IloNumArray& xstar, IloNumArray& ystar, IloNumArray& zstar) {
+  int edge_counter = 0;
+  vector<vector<int> > g(v_, vector<int>());
+  for (int i = 0; i < a_; i++) {
+    if (xstar[i] > 1 - EPSILON) {
+      edge_counter++;
+      if (zstar[2 * i] + zstar[2 * i + 1] <= 1 - EPSILON) return 1;
+      if (zstar[2 * i] + zstar[2 * i + 1] >= 1 + EPSILON) return 1;
+      g[origem[i]].push_back(destino[i]);
+      g[destino[i]].push_back(origem[i]);
+    }
+  }
+  if (edge_counter != v_ - 1) return 2;
+
+  int y_counter = 0;
+  int branch_counter = 0;
+  for (int i = 0; i < v_; i++) {
+    if (ystar[i] > 1 - EPSILON) y_counter++;
+    if (g[i].size() > 2) {
+      branch_counter++;
+    }
+  }
+  if (y_counter != branch_counter) return 3;
+
+  vector<bool> used(v_, false);
+  queue<int> myq;
+  myq.push(0);
+  used[0] = true;
+  while (!myq.empty()) {
+    int p = myq.front(); myq.pop();
+    for (int i = 0; i < g[p].size(); i++) {
+      int to = g[p][i];
+      if (used[to]) continue;
+      used[to] = true;
+      myq.push(to);
+    }
+  }
+  for (int i = 0; i < v_; i++) {
+    if (!used[i]) return 4;
+  }
+  return 0;
+}
+
+/* Corte baseado em min-cut do grafo
+  O algoritmo utilizado é o Karger's Algorithm, onde
+  repete-se o seguinte procedimento diversas vezes:
+    - Contrair duas arestas aleatorias repetidamente
+      ate obter apenas dois vertices
+    - Soma o peso de todas arestas com um extremo em
+      cada um dos dois vertices resultantes
+    - Verifica se essa soma de pesos eh menor que um
+    - Caso seja, o corte eh adicionado*/
+ILOUSERCUTCALLBACK1(CorteMinimo, IloBoolVarArray, x) {
+  cout << "Entrei no callback" << endl;
+
+   /* Recupera ambiente do cplex */
+  IloEnv env = getEnv();
+
+  /* Pega a solução do LP. */
+  IloNumArray val(env);
+  getValues(val, x);
+
+  vector<int> r(v_);
+  vector<int> p(v_);
+  for (int i = 0; i < 1000; i++) {
+    for (int j = 0; j < v_; j++) makeSet(j, r, p);
+    int n_componentes = v_;
+    /* Contracao de arestas ate so restar dois vertices */
+    while (n_componentes > 2) {
+      int aresta = rand() % a_;
+      n_componentes -= uniteSets(origem[aresta], destino[aresta], r, p);
+    }
+
+    double cut = 0;
+    for (int j = 0; j < a_; j++) {
+      /* Somar arestas ligando os dois vertices resultantes */
+      if (findSet(origem[j], r, p) != findSet(destino[j], r, p))
+        cut += val[j];
+    }
+
+    /* Adicionando corte */
+    if (cut < 1) {
+      cout << "Cut pequeno encontrado na iteraçao: " << i << " cut: " << cut << endl;
+      for (int j = 0; j < v_; j++) {
+        cout << j << ": " << findSet(j, r, p) << endl;
+      }
+      IloExpr cut(env);
+      for (int j = 0; j < a_; j++) {
+        if (findSet(origem[j], r, p) != findSet(destino[j], r, p)) {
+          cut += x[j];
+        }
+      }
+      add(cut >= 1);
+      break;
+    }
+  }
+}
+
 int main(int argc, char * argv[]) {
 
   /* variaveis auxiliares */
@@ -65,6 +199,7 @@ int main(int argc, char * argv[]) {
   /* le dados de entrada do problema da AGMR.*/
   fin >> v_ >> a_;
 
+  int root = 0; // No raiz, escolhido arbitrariamente
   vector<vector<int> > g(v_, vector<int>());
   map<pair<int, int>, int> edge_to_index;
   v.resize(v_);
@@ -83,16 +218,13 @@ int main(int argc, char * argv[]) {
   /* objeto que representa o modelo */
   IloModel model(env);
 
-  int root = 0;
-  map<int, IloBoolVar*> y_map;
+  IloBoolVarArray x(env, a_);
   IloBoolVarArray y(env, v_);
-  for(int i = 0; i < v_; i++){
-    y_map[i] = &y[i];
-  }
+  IloBoolVarArray z(env, 2 * a_);
   map<pair<int, int>, IloBoolVar*> x_map;
   map<pair<int, int>, IloBoolVar*> z_map;
-  IloBoolVarArray x(env, a_);
-  IloBoolVarArray z(env, 2 * a_);
+  map<int, IloBoolVar*> y_map;
+  for(int i = 0; i < v_; i++) y_map[i] = &y[i];
   for(int i = 0; i < a_; i++){
     x_map[{origem[i], destino[i]}] = &x[i];
     z_map[{origem[i], destino[i]}] = &z[2 * i];
@@ -171,6 +303,8 @@ int main(int argc, char * argv[]) {
   /* salva um arquivo ".lp" com o LP original */
   cplex.exportModel("LP.lp");
 
+  cplex.use(CorteMinimo(env, x));
+
   cplex.solve();
 
   cout << "solucao otima: " << cplex.getObjValue() << endl;
@@ -188,6 +322,16 @@ int main(int argc, char * argv[]) {
   for(int i = 0; i < a_; i++){
     cout << "x[" << origem[i] << "," << destino[i] << "]: " << xstar[i] << endl;
   }
-  
+
+  IloNumArray zstar(env);
+  cplex.getValues(zstar, z);
+  cout << "valores de z:" << endl;
+  for (int i = 0; i < a_; i++) {
+    cout << "(" << origem[i] << ", " << destino[i] << ") " << zstar[2 * i] << endl;
+    cout << "(" << destino[i] << ", " << origem[i] << ") " << zstar[2 * i + 1] << endl;
+  }
+
+  cout << "test return code: " << testOk(xstar, ystar, zstar) << endl;
+
   return 0;
 }
