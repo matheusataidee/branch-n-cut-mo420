9d1dec08a552c807d6c37f4f8c550aa98dde1605
Matheus Ataide <matheusataide95@gmail.com>
2019-12-04 02:58:18 -0300

    Adding ILP implementation

---
+++ inc/ilp.hpp
@@ -0,0 +1,323 @@
+#ifndef ILP_H_
+#define ILP_H_
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+
+#include "ilcplex/ilocplex.h"
+#include "utils.hpp"
+
+using namespace std;
+
+extern int v_;
+extern int a_;
+extern vector<int> origem;
+extern vector<int> destino;
+extern vector<vector<int> > g;
+extern map<pair<int, int>, int> edge_to_index;
+
+extern map<int, IloBoolVar*> y; /* dicionario de ponteiros para as variaveis Y */
+extern map<std::string, IloBoolVar*> x; /* dicionario de ponteiros para as variaveis X */  
+extern vector<int> arestas_ponte; /* armazena as pontes encontradas no pre processamento */
+extern vector<int> vertices_corte; /* armazena os vertices de corte encontrados no pre processamento */
+
+/* declaracao das necessidades do pre processamento */
+extern vector<vector<int>> listaAdj;
+extern int tempo_global;
+extern vector<bool> visitado;
+extern vector<int> tempo_entrada; /* Tempo que a árvore DFS chega em v */
+extern vector<int> menor_retorno; /* Menor tempo de entrada que v alcança na árvore DFS */
+
+// void pre_processamento();
+// void dfs_cortes(int v, int pai = -1);
+
+extern IloBoolVarArray* y_global;     
+extern IloBoolVarArray* x_global;     
+
+/* ---- conjunto de variaveis ainda nao utilizadas devidamente (estavam no exemplo) ---- */
+	
+	extern int totcuts; /* contador do total de cortes por nó da arvore B&B */
+	extern int itersep; /* contador do total de lacos de separacao de cortes por nó da arvore B&B */
+	extern int MAX_NODE_DEPTH_FOR_SEP; /* profundidade maxima de um no da arvore de B&B em que sera feita separacao */
+	extern double objval_relax; /* valor otimo da primeira relaxacao */
+	extern double objval_node1; /* valor da relaxacao linear no final do 1o nó */
+
+	extern double zstar; /* melhor limitante primal encontrado */
+	extern double melhor_limitante_dual; /* melhor limitante dual encontrado */
+	extern int incumbent_node; /* no do melhor limitante primal encontrado */
+
+	extern int contador_sec;
+	extern int contador_d18;
+	extern int contador_d19;
+	extern int contador_d34;
+	extern int total_no_exp;
+
+	extern IloInt node_depth; /* profundidade do nó */
+
+ILOUSERCUTCALLBACK1(CortesILP, IloBoolVarArray, x) {
+
+/*	cout << "callback executado!" << endl; */
+
+	vector<vector<int>> components;
+	vector<int> inserted(v_, 0);
+	int alocado = 0;
+	int novo;
+	int size_comp = 1;
+
+	vector<int> row;
+
+/*  recupera o ambiente do cplex */
+	IloEnv env = getEnv();
+
+/*  pega a solução vigente */
+	IloNumArray val(env);
+	getValues(val, x);
+
+	IloNumArray y_val(env);
+	getValues(y_val, *y_global);
+
+/*	cria os cortes representados pelas desigualdades validas do tipo (18) */
+	for(int i = 0; i < v_; i++){
+		vector<int> arestas;
+		for(int e = 0; e < a_; e++)
+			if(val[e] > 1.0 - EPSILON && (i == origem[e] || i == destino[e])) arestas.push_back(e);
+
+		if(arestas.size() > 2 && y_val[i] < 1.0 - EPSILON){
+			IloExpr cut(env);
+			for(int value : arestas) cut += x[value];
+			double lhs = double(arestas.size() - 2);
+//			cout << cut << " - " << lhs << " * " << *y[i] << " <= " << 2 <<  endl; 
+			add(cut - lhs * *y[i] <= 2);
+			contador_d18++;
+		}
+	}
+/*  fim do corte pelas desigualdades (18) */
+
+/*  avalia se o grafo retornado tem mais de uma componente e, se sim, separa essas componentes */
+	do{
+
+		if(row.size() == 0){
+
+/*			cout << "component novo criado" << endl; */
+
+			for(int k = 0; k < v_; k++){
+
+				if(inserted[k] == 0){
+					row.push_back(k);
+					inserted[k] = 1;
+					alocado++;
+					break;
+				}
+			}
+		}else{
+
+			novo = 1;
+
+			for(int k : row){
+				for(int e = 0; e < a_; e++){
+
+					if(k == origem[e] && val[e] == 1 && inserted[destino[e]] == 0){
+/*						cout << "["<< origem[e] << "," << destino[e] << "]" << endl; */
+						row.push_back(destino[e]);
+						inserted[destino[e]] = 1;
+						alocado++;
+						novo = 0;
+					}
+
+					if(k == destino[e] && val[e] == 1 && inserted[origem[e]] == 0){
+/*						cout << "[" << origem[e] << "," << destino[e] << "]" << endl; */
+						row.push_back(origem[e]);
+						inserted[origem[e]] = 1;
+						alocado++;
+						novo = 0;
+					}
+				}
+			}
+
+			if(novo){
+				components.push_back(row);
+				row.clear();
+				size_comp++;
+			}
+		}
+
+	}while(alocado < v_);
+
+	components.push_back(row);
+
+/*	avaliar em qual componente o vertice do corte esta e se ele esta em posicao de separacao [preparacao para (19)] */
+	if(size_comp == 2){
+		int position = -1;
+		for(const std::vector<int> &component : components){
+			position++;
+			if(component.size() > 2){
+				for(int cut_vertex : vertices_corte){
+					if(std::find(component.begin(), component.end(), cut_vertex) != component.end() && y_val[cut_vertex] < 1){
+
+						int index;
+						if(position == 0)
+							index = 1;
+						else if (position == 1)
+							index = 0;
+
+						int separado = 0;
+						for(int e = 0; e < a_; e++){
+							if(origem[e] == cut_vertex){
+								if(std::find(components[index].begin(), components[index].end(), destino[e]) != components[index].end()) separado = 1;
+							}else if(destino[e] == cut_vertex){
+								if(std::find(components[index].begin(), components[index].end(), origem[e]) != components[index].end()) separado = 1;
+							}
+						}
+
+						if(separado){
+							for(int v1 = 0; v1 < component.size() - 1; v1++){
+								for(int v2 = v1 + 1; v2 < component.size(); v2++){
+
+									int encontrado = 0;
+									IloExpr cut(env);
+
+									for(int e = 0; e < a_; e++){
+										if(origem[e] == cut_vertex && destino[e] == component[v1]){ 
+											cut += x[e];
+											encontrado++;
+										}
+										if(destino[e] == cut_vertex && origem[e] == component[v1]){
+											cut += x[e];
+											encontrado++;
+										}
+										if(origem[e] == cut_vertex && destino[e] == component[v2]){
+											cut += x[e];
+											encontrado++;
+										}
+										if(destino[e] == cut_vertex && origem[e] == component[v2]){
+											cut += x[e];
+											encontrado++;
+										}
+									}
+/*								cria os cortes representados pelas desigualdades validas do tipo (19) */
+								if(encontrado == 2 && y_val[cut_vertex] < 1 - EPSILON){
+										add(cut - *y[cut_vertex] - 1 <= 0);
+										contador_d19++;
+//										for(int z : component) cout << z << " ";
+//										cout << endl << "vertices: " << cut_vertex << ", (" << component[v1] << ", " << component[v2] << ")" << endl;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
+
+ILOLAZYCONSTRAINTCALLBACK1(LazyConstraintsILP, IloBoolVarArray, x) {
+
+/*	recupera ambiente do cplex */
+	IloEnv env = getEnv();
+
+/*	pega a solução do LP. */
+	IloNumArray val(env);
+	getValues(val, x);
+
+	vector<int> r(v_);
+	vector<int> p(v_);
+
+	for(int i = 0; i < 1000; i++){
+
+		for (int j = 0; j < v_; j++) makeSet(j, r, p);
+		int n_componentes = v_;
+
+/*		Contracao de arestas ate so restar dois vertices */
+		while(n_componentes > 2){
+			int aresta = rand() % a_;
+			n_componentes -= uniteSets(origem[aresta], destino[aresta], r, p);
+		}
+
+		double cut = 0;
+		for(int j = 0; j < a_; j++){
+/* 			somar arestas ligando os dois vertices resultantes */
+			if(findSet(origem[j], r, p) != findSet(destino[j], r, p))
+				cut += val[j];
+		}
+
+/* 		adicionando corte */
+		if(cut < 1){
+			IloExpr cut(env);
+			for(int j = 0; j < a_; j++)
+				if(findSet(origem[j], r, p) != findSet(destino[j], r, p))
+					cut += x[j];
+
+			add(cut >= 1);
+			contador_sec++;
+			break;
+		}
+	}
+
+}
+
+ILOHEURISTICCALLBACK2(HeuristicaPrimalILP, IloBoolVarArray, y, IloBoolVarArray, x) {
+
+	IloNumArray val_x(getEnv());
+	IloNumArray val_y(getEnv());
+	getValues(val_x, x);
+	getValues(val_y, y);
+
+//  cout << "testOk() pre heuristica: " << testOk(val_x, val_y, val_z) << endl;
+  /* r e p sao vetores uteis para o algoritmo union-find */
+	vector<int> r(v_);
+	vector<int> p(v_);
+	vector<int> deg(v_, 0); /* grau dos vertices da solucao heuristica */
+	vector<bool> used(v_, false); /* util para marcar vars z durante bfs*/
+	vector<RegAux> arestas_ordenadas(a_);
+
+	for (int i = 0; i < v_; i++) makeSet(i, r, p);
+	for (int i = 0; i < a_; i++) {
+		arestas_ordenadas[i].valor = val_x[i];
+		arestas_ordenadas[i].indice = i;
+	}
+
+	sort(arestas_ordenadas.begin(), arestas_ordenadas.end());
+  /* Inicialmente cada vertice eh uma componente */
+	int n_components = v_;
+	for (int i = 0; i < a_; i++) {
+		int id = arestas_ordenadas[i].indice;
+			/* Caso em que a aresta conecta duas componentes */
+		if (uniteSets(origem[id], destino[id], r, p)) {
+			n_components--;
+			deg[origem[id]]++;  deg[destino[id]]++;
+			val_x[id] = 1.0;
+		}else{
+			val_x[id] = 0.0;
+		}
+	}
+	double custo = 0;
+  /* Demarca variaveis y correspondentes a vertices de ramificacoes */
+	for (int i = 0; i < v_; i++) {
+		if (deg[i] >= 3) {
+				val_y[i] = 1.0;
+				custo += 1;
+			}else{
+				val_y[i] = 0.0;
+		}
+	}
+
+	IloNumVarArray vars(getEnv());
+	IloNumArray vals(getEnv());
+
+	for (int i = 0; i < a_; i++) vars.add(x[i]);
+	for (int i = 0; i < v_; i++) vars.add(y[i]);
+
+	for (int i = 0; i < a_; i++) vals.add(val_x[i]);
+	for (int i = 0; i < v_; i++) vals.add(val_y[i]);
+
+	setSolution(vars, vals, custo);
+}
+
+
+#endif  // ILP_H_
--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 
 #include "utils.hpp"
 #include "hybrid.hpp"
+#include "ilp.hpp"
 
 #include "ilcplex/ilocplex.h"
 
@@ -66,178 +67,352 @@ IloBoolVarArray* x_global;
 	IloInt node_depth; /* profundidade do nó */
 
 int main(int argc, char * argv[]) {
-
-  /* variaveis auxiliares */
-  /* - diz se usara ou nao a heuristica primal */
-  ModelType model_type;
-  double timelimit;
-  bool use_primal_heur;
-
-  if (argc < 5 || argc > 5) {
-    cout << "Usage: " << string(argv[0]) << " <model> <time-limit> <heur-primal> <arq-input>" << endl;
-    cout << "\t<model>: s for ILP, h for HYBRID" << endl;
-    cout << "\t<time-limit>: Time limit in seconds" << endl;
-    cout << "\t<heur-primal>: 1 for using, 0 for not using" << endl;
-    cout << "\t<arq-input>: path to input instance" << endl;
-    return -1;
-  }
-  model_type = (string(argv[1]) == "s") ? ILP : HYBRID;
-  timelimit = atof(argv[2]);
-  use_primal_heur = (string(argv[3]) == "1");
-  ifstream fin(argv[4]);
-
-  /* ambiente do cplex */
-  IloEnv env;
-
-  /* le dados de entrada do problema da AGMR.*/
-  fin >> v_ >> a_;
-
-  int root = 0; // No raiz, escolhido arbitrariamente
-  g = vector<vector<int> >(v_, vector<int>());
-  v.resize(v_);
-  origem.resize(a_);
-  destino.resize(a_);
-
-  for (int i = 0; i < a_; i++) {
-    fin >> origem[i] >> destino[i];
-    origem[i]--;  destino[i]--; // Tornando a indexação 0-based
-    g[origem[i]].push_back(destino[i]);
-    g[destino[i]].push_back(origem[i]);
-    edge_to_index[{origem[i], destino[i]}] = 2 * i;
-    edge_to_index[{destino[i], origem[i]}] = 2 * i + 1;
-  }
-
-  /* objeto que representa o modelo */
-  IloModel model(env);
-
-  IloBoolVarArray x(env, a_);
-  IloBoolVarArray y(env, v_);
-  IloBoolVarArray z(env, 2 * a_);
-  map<pair<int, int>, IloBoolVar*> x_map;
-  map<pair<int, int>, IloBoolVar*> z_map;
-  map<int, IloBoolVar*> y_map;
-  for(int i = 0; i < v_; i++) y_map[i] = &y[i];
-  for(int i = 0; i < a_; i++){
-    x_map[{origem[i], destino[i]}] = &x[i];
-    z_map[{origem[i], destino[i]}] = &z[2 * i];
-    z_map[{destino[i], origem[i]}] = &z[2 * i + 1];
-  }
-
-  /* funcao objetivo (1) */
-  IloExpr obj(env);
-  for(int i = 0; i < v_; i++)
-    obj += y[i];
-  model.add(IloMinimize(env, obj)); 
-
-  /* restricao (3) */
-  IloExpr constr_3(env);
-  for(int i = 0; i < a_; i++)
-    constr_3 += x[i];
-  model.add(constr_3 == v_ - 1);
-
-  /* restricao (4) */
-  for(int i = 0; i < v_; i++){
-    IloExpr constr_4(env);
-    for (int e = 0; e < g[i].size(); e++) {
-      int to = g[i][e];
-      constr_4 += x[edge_to_index[{i, to}] / 2];
-    }
-    model.add(constr_4 - 2 <= ((int)g[i].size() - 2) * y[i]);
-  }
-
-  /* restricao (27) */
-  for (int i = 0; i < v_; i++) {
-    if (i == root) continue;
-    IloExpr constr_27(env);
-    for (int e = 0; e < g[i].size(); e++) {
-      int to = g[i][e];
-      /* Somando arestas de entrada */
-      constr_27 += z[edge_to_index[{to, i}]];
-    }
-    model.add(constr_27 == 1);
-  }
-
-  /* restricao (28) */
-  for (int i = 0; i < v_; i++) {
-    if (i == root) continue;
-    IloExpr constr_28(env);
-    for (int e = 0; e < g[i].size(); e++) {
-      int to = g[i][e];
-      /* Somando arestas de saida */
-      constr_28 += z[edge_to_index[{i, to}]];
-    }
-    model.add(constr_28 - 1 <= ((int)g[i].size() - 2) * y[i]);
-  }
-
-  /* restricao (29) */
-  IloExpr constr_29(env);
-  for (int e = 0; e < g[root].size(); e++) {
-    int to = g[root][e];
-    constr_29 += z[edge_to_index[{root, to}]];
-  }
-  model.add(constr_29 - 2 <= ((int)g[root].size() - 2) * y[root]);
-
-  /* restricao (35) */
-  IloExpr constr_35(env);
-  for (int i = 0; i < a_; i++) {
-    IloExpr constr_35(env);
-    constr_35 += z[2 * i];
-    constr_35 += z[2 * i + 1];
-    model.add(constr_35 == x[i]);
-  }
-
-  /* cria objeto do cplex */
-  IloCplex cplex(env);
-
-  /* carrega o modelo */
-  cplex.extract(model);
-
-  /*  atribui valores aos diferentes parametros de controle do CPLEX */
-	cplex.setParam(IloCplex::Param::TimeLimit, timelimit);
-	cplex.setParam(IloCplex::Param::Preprocessing::Presolve, CPX_OFF);
-	cplex.setParam(IloCplex::Param::MIP::Strategy::HeuristicFreq, -1);
-	cplex.setParam(IloCplex::Param::MIP::Strategy::RINSHeur, -1);
-	cplex.setParam(IloCplex::Param::MIP::Strategy::FPHeur, -1);
-	cplex.setParam(IloCplex::Param::Preprocessing::Linear, 0);
-	cplex.setParam(IloCplex::Param::MIP::Strategy::Search, CPX_MIPSEARCH_TRADITIONAL);
-	cplex.setParam(IloCplex::Param::Threads, 1);
-	cplex.setParam(IloCplex::Param::MIP::Limits::CutsFactor, 1.0);
   
-  /* salva um arquivo ".lp" com o LP original */
-  cplex.exportModel("LP.lp");
+  chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
+
+/*  variaveis auxiliares */
+	ModelType model_type;
+	double timelimit;
+	string input_path;
+
+/*  informa se usara ou nao a heuristica primal */
+	bool use_primal_heur;
+
+/*  define o ambiente do cplex */
+	IloEnv env;
+
+	if(argc < 5 || argc > 5){
+		cout << "Usage: " << string(argv[0]) << " <model> <time-limit> <heur-primal> <arq-input>" << endl;
+		cout << "\t<model>: s for ILP, h for HYBRID" << endl;
+		cout << "\t<time-limit>: Time limit in seconds" << endl;
+		cout << "\t<heur-primal>: 1 for using, 0 for not using" << endl;
+		cout << "\t<arq-input>: path to input instance" << endl;
+		return -1;
+	}else{
+		model_type = (string(argv[1]) == "s") ? ILP : HYBRID;
+		timelimit = atof(argv[2]);
+		use_primal_heur = (string(argv[3]) == "1");
+		input_path = string(argv[4]);
+	}
+
+  if (model_type == ILP) {
+
+        cout << input_path << endl;
+
+    /*  inicializa valores de variaveis globais de corte e separacao */
+      totcuts = 0;   
+      itersep = 0;
+
+    //	cout << "[Entrar com os dados da instancia: (copiar e colar todos os valores)]" << endl;
+
+      ifstream file(input_path);
+
+      int descarta;
+
+    /*  le dados de entrada do problema */
+    //	scanf("%d %d", &v_, &a_);
+      file >> v_ >> a_ >> descarta;
+      v.resize(v_);
+      origem.resize(a_);
+      destino.resize(a_);
+      listaAdj.resize(v_);
+
+    //	iota(v.begin(), v.end(), 0);
+
+    //	for(int i = 0; i < v_; i++) scanf("%d", &v[i]);
+      for(int i = 0; i < v_; i++) v[i] = i;
+
+    //	for(int i = 0; i < a_; i++) scanf("%d %d", &origem[i], &destino[i]);
+
+      int orig, dest;
+      for(int i = 0; i < a_; i++){
+    //		scanf("%d %d", &orig, &dest);
+        file >> orig >> dest >> descarta;
+
+        origem[i] = orig - 1;
+        destino[i] = dest - 1;
+
+        listaAdj[orig - 1].push_back(dest - 1);
+        listaAdj[dest - 1].push_back(orig - 1);
+      }
+
+      file.close();
+
+        pre_processamento();
+
+    //	for(int value : arestas_ponte) cout << "aresta ponte: " << value + 14 << " - (" << origem[value] << ", " << destino[value] << ")" << endl;
+    //	for(int value : vertices_corte) cout << "vertices corte: " << value << endl;
+
+      if(model_type) return -1;
+
+    /*  objeto que representa o modelo */
+      IloModel model(env);
+
+      IloBoolVarArray y_temp(env, v_);
+      y_global = &y_temp;
+      for(int i = 0; i < v_; i++){
+        y[i] = &y_temp[i];
+      }
+
+      IloBoolVarArray x_temp(env, a_);
+      x_global = &x_temp;
+      for(int i = 0; i < a_; i++){
+        x["" + to_string(origem[i]) + "-" + to_string(destino[i]) + ""] = &x_temp[i];
+      }
+
+    /*  define a funcao objetivo (1) */
+      IloExpr obj(env);
+      for(int i = 0; i < v_; i++)
+        obj += *y[i];
+      model.add(IloMinimize(env, obj)); 
+
+    /*  cria restricao (3) */
+      IloExpr constr_3(env);
+      for(int i = 0; i < a_; i++)
+        constr_3 += *x[to_string(origem[i]) + "-" + to_string(destino[i])];
+      model.add(constr_3 == v_ - 1);
+
+    /*  cria familia de restricoes (4) */
+      for(int i = 0; i < v_; i++){
+        IloExpr constr_4(env);
+        for(int e = 0; e < a_; e++){
+          if(i == origem[e]) constr_4 += *x[to_string(i) + "-" + to_string(destino[e])];
+          if(i == destino[e]) constr_4 += *x[to_string(origem[e]) + "-" + to_string(i)];
+        }
+        model.add(constr_4 - 2 - 7 * *y[i] <= 0); /* ajustar o valor 100 para d(v) */
+      } 
+
+    //	for(int value : arestas_ponte) model.add(x_temp[value] == 1);
+
+    /*  cria objeto do cplex */
+      IloCplex cplex(env);
+
+    /*  carrega o modelo */
+      cplex.extract(model);
+
+    /*  silencia o cplex no terminal */
+    //	cplex.setOut(env.getNullStream());
+
+    /*  atribui valores aos diferentes parametros de controle do CPLEX */
+      cplex.setParam(IloCplex::Param::TimeLimit, timelimit);
+      cplex.setParam(IloCplex::Param::Preprocessing::Presolve, CPX_OFF);
+      cplex.setParam(IloCplex::Param::MIP::Strategy::HeuristicFreq, -1);
+      cplex.setParam(IloCplex::Param::MIP::Strategy::RINSHeur, -1);
+      cplex.setParam(IloCplex::Param::MIP::Strategy::FPHeur, -1);
+      cplex.setParam(IloCplex::Param::Preprocessing::Linear, 0);
+      cplex.setParam(IloCplex::Param::MIP::Strategy::Search, CPX_MIPSEARCH_TRADITIONAL);
+      cplex.setParam(IloCplex::Param::Threads, 1);
+      cplex.setParam(IloCplex::Param::MIP::Limits::CutsFactor, 1.0);
+
+    /*  salva um arquivo ".lp" com o LP original */
+      cplex.exportModel("LP.lp");
+
+      if(use_primal_heur == 1) cplex.use(HeuristicaPrimalILP(env, y_temp, x_temp));
+      cplex.use(LazyConstraintsILP(env, x_temp));
+      cplex.use(CortesILP(env, x_temp));
+
+      bool achou_solucao;
+
+      if(cplex.solve()){
+
+        incumbent_node = cplex.getIncumbentNode();
+        zstar = cplex.getObjValue();
+        melhor_limitante_dual = cplex.getBestObjValue();
+        total_no_exp = cplex.getNnodes();
+
+      //	cout << "solucao otima: " << zstar << endl << endl;
+
+        IloNumArray ystar(env);
+        cplex.getValues(ystar, y_temp);
+      /*	cout << "valores de y:" << endl;
+        for(int i = 0; i < v_; i++){
+          if(ystar[i] > 0) cout << "\ty[" << i << "]: " << ystar[i] << endl;
+        } */
+
+        IloNumArray xstar(env);
+        cplex.getValues(xstar, x_temp);
+      /*	cout << endl << "valores de x:" << endl;
+        for(int i = 0; i < a_; i++){
+          if(xstar[i] > 0) cout << "\tx[" << origem[i] << "," << destino[i] << "]: " << xstar[i] << endl;
+        } */
+
+        chrono::high_resolution_clock::time_point now = chrono::high_resolution_clock::now();
+        chrono::duration<double> diff_time = chrono::duration_cast<chrono::duration<double>>(now - start);
+
+        imprime_solucao(model_type, timelimit, use_primal_heur, input_path, xstar, diff_time.count());
+
+      }else{
+        printf("Main: programa terminou sem achar solucao inteira !\n");
+
+        incumbent_node = -1;
+        zstar = -1;
+        melhor_limitante_dual = cplex.getBestObjValue();
+        total_no_exp = cplex.getNnodes();
+
+        chrono::high_resolution_clock::time_point now = chrono::high_resolution_clock::now();
+        chrono::duration<double> diff_time = chrono::duration_cast<chrono::duration<double>>(now - start);
+
+        
+        imprime_solucao(model_type, timelimit, use_primal_heur, input_path, diff_time.count());
+      }
+
+  } else if (model_type == HYBRID) {
+
+    ifstream fin(argv[4]);
+
+    /* ambiente do cplex */
+    IloEnv env;
+
+    /* le dados de entrada do problema da AGMR.*/
+    fin >> v_ >> a_;
+
+    int root = 0; // No raiz, escolhido arbitrariamente
+    g = vector<vector<int> >(v_, vector<int>());
+    v.resize(v_);
+    origem.resize(a_);
+    destino.resize(a_);
+
+    for (int i = 0; i < a_; i++) {
+      fin >> origem[i] >> destino[i];
+      origem[i]--;  destino[i]--; // Tornando a indexação 0-based
+      g[origem[i]].push_back(destino[i]);
+      g[destino[i]].push_back(origem[i]);
+      edge_to_index[{origem[i], destino[i]}] = 2 * i;
+      edge_to_index[{destino[i], origem[i]}] = 2 * i + 1;
+    }
+
+    /* objeto que representa o modelo */
+    IloModel model(env);
+
+    IloBoolVarArray x(env, a_);
+    IloBoolVarArray y(env, v_);
+    IloBoolVarArray z(env, 2 * a_);
+    map<pair<int, int>, IloBoolVar*> x_map;
+    map<pair<int, int>, IloBoolVar*> z_map;
+    map<int, IloBoolVar*> y_map;
+    for(int i = 0; i < v_; i++) y_map[i] = &y[i];
+    for(int i = 0; i < a_; i++){
+      x_map[{origem[i], destino[i]}] = &x[i];
+      z_map[{origem[i], destino[i]}] = &z[2 * i];
+      z_map[{destino[i], origem[i]}] = &z[2 * i + 1];
+    }
+
+    /* funcao objetivo (1) */
+    IloExpr obj(env);
+    for(int i = 0; i < v_; i++)
+      obj += y[i];
+    model.add(IloMinimize(env, obj)); 
+
+    /* restricao (3) */
+    IloExpr constr_3(env);
+    for(int i = 0; i < a_; i++)
+      constr_3 += x[i];
+    model.add(constr_3 == v_ - 1);
+
+    /* restricao (4) */
+    for(int i = 0; i < v_; i++){
+      IloExpr constr_4(env);
+      for (int e = 0; e < g[i].size(); e++) {
+        int to = g[i][e];
+        constr_4 += x[edge_to_index[{i, to}] / 2];
+      }
+      model.add(constr_4 - 2 <= ((int)g[i].size() - 2) * y[i]);
+    }
+
+    /* restricao (27) */
+    for (int i = 0; i < v_; i++) {
+      if (i == root) continue;
+      IloExpr constr_27(env);
+      for (int e = 0; e < g[i].size(); e++) {
+        int to = g[i][e];
+        /* Somando arestas de entrada */
+        constr_27 += z[edge_to_index[{to, i}]];
+      }
+      model.add(constr_27 == 1);
+    }
+
+    /* restricao (28) */
+    for (int i = 0; i < v_; i++) {
+      if (i == root) continue;
+      IloExpr constr_28(env);
+      for (int e = 0; e < g[i].size(); e++) {
+        int to = g[i][e];
+        /* Somando arestas de saida */
+        constr_28 += z[edge_to_index[{i, to}]];
+      }
+      model.add(constr_28 - 1 <= ((int)g[i].size() - 2) * y[i]);
+    }
+
+    /* restricao (29) */
+    IloExpr constr_29(env);
+    for (int e = 0; e < g[root].size(); e++) {
+      int to = g[root][e];
+      constr_29 += z[edge_to_index[{root, to}]];
+    }
+    model.add(constr_29 - 2 <= ((int)g[root].size() - 2) * y[root]);
+
+    /* restricao (35) */
+    IloExpr constr_35(env);
+    for (int i = 0; i < a_; i++) {
+      IloExpr constr_35(env);
+      constr_35 += z[2 * i];
+      constr_35 += z[2 * i + 1];
+      model.add(constr_35 == x[i]);
+    }
+
+    /* cria objeto do cplex */
+    IloCplex cplex(env);
+
+    /* carrega o modelo */
+    cplex.extract(model);
+
+    /*  atribui valores aos diferentes parametros de controle do CPLEX */
+    cplex.setParam(IloCplex::Param::TimeLimit, timelimit);
+    cplex.setParam(IloCplex::Param::Preprocessing::Presolve, CPX_OFF);
+    cplex.setParam(IloCplex::Param::MIP::Strategy::HeuristicFreq, -1);
+    cplex.setParam(IloCplex::Param::MIP::Strategy::RINSHeur, -1);
+    cplex.setParam(IloCplex::Param::MIP::Strategy::FPHeur, -1);
+    cplex.setParam(IloCplex::Param::Preprocessing::Linear, 0);
+    cplex.setParam(IloCplex::Param::MIP::Strategy::Search, CPX_MIPSEARCH_TRADITIONAL);
+    cplex.setParam(IloCplex::Param::Threads, 1);
+    cplex.setParam(IloCplex::Param::MIP::Limits::CutsFactor, 1.0);
+    
+    /* salva um arquivo ".lp" com o LP original */
+    cplex.exportModel("LP.lp");
 
 
-  cplex.use(HeuristicaPrimal(env, x, y, z));
-	cplex.use(LazyConstraints(env, x));
-  cplex.use(CorteMinimo(env, x));
+    cplex.use(HeuristicaPrimal(env, x, y, z));
+    cplex.use(LazyConstraints(env, x));
+    cplex.use(CorteMinimo(env, x));
 
-  cplex.solve();
+    cplex.solve();
 
-  cout << "solucao otima: " << cplex.getObjValue() << endl;
+    cout << "solucao otima: " << cplex.getObjValue() << endl;
 
-  IloNumArray ystar(env);
-  cplex.getValues(ystar, y);
-  cout << "valores de y:" << endl;
-  for(int i = 0; i < v_; i++){
-    cout << "y[" << i << "]: " << ystar[i] << endl;
+    IloNumArray ystar(env);
+    cplex.getValues(ystar, y);
+    cout << "valores de y:" << endl;
+    for(int i = 0; i < v_; i++){
+      cout << "y[" << i << "]: " << ystar[i] << endl;
+    }
+
+    IloNumArray xstar(env);
+    cplex.getValues(xstar, x);
+    cout << "valores de x:" << endl;
+    for(int i = 0; i < a_; i++){
+      cout << "x[" << origem[i] << "," << destino[i] << "]: " << xstar[i] << endl;
+    }
+
+    IloNumArray zstar(env);
+    cplex.getValues(zstar, z);
+    cout << "valores de z:" << endl;
+    for (int i = 0; i < a_; i++) {
+      cout << "(" << origem[i] << ", " << destino[i] << ") " << zstar[2 * i] << endl;
+      cout << "(" << destino[i] << ", " << origem[i] << ") " << zstar[2 * i + 1] << endl;
+    }
+
+    cout << "test return code: " << testOk(xstar, ystar, zstar) << endl;
   }
-
-  IloNumArray xstar(env);
-  cplex.getValues(xstar, x);
-  cout << "valores de x:" << endl;
-  for(int i = 0; i < a_; i++){
-    cout << "x[" << origem[i] << "," << destino[i] << "]: " << xstar[i] << endl;
-  }
-
-  IloNumArray zstar(env);
-  cplex.getValues(zstar, z);
-  cout << "valores de z:" << endl;
-  for (int i = 0; i < a_; i++) {
-    cout << "(" << origem[i] << ", " << destino[i] << ") " << zstar[2 * i] << endl;
-    cout << "(" << destino[i] << ", " << origem[i] << ") " << zstar[2 * i + 1] << endl;
-  }
-
-  cout << "test return code: " << testOk(xstar, ystar, zstar) << endl;
 
   return 0;
 }
